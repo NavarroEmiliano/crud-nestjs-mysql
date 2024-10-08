017f53297ff44fd305744caf4d0d0dc1bfaa5df3
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-10-07 23:52:37 -0300

    login service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,19 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
 import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService, private readonly jwtService: JwtService) {}
 
   async register(registerDto: RegisterDto) {
     const userFound = await this.usersService.findOneByEmail(registerDto.email);
 
     if (userFound) {
-      throw new ConflictException('User already exists');
+      throw new ConflictException('user already exists');
     }
 
     return await this.usersService.create({
@@ -20,7 +22,19 @@ export class AuthService {
     });
   }
 
-  login() {
+  async login(loginDto: LoginDto) {
+    const userFound = await this.usersService.findOneByEmail(loginDto.email);
+
+    if(!userFound){
+      throw new UnauthorizedException('email or password is wrong')
+    }
+    const isPasswordValid = await bcrypt.compare(userFound.password,loginDto.password)
+
+    if(!isPasswordValid) {
+      throw new UnauthorizedException('password is wrong')
+    }
+
+    const payload = await {}
     return;
   }
 }
